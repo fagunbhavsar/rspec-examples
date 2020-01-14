c5a5f7cb48e9103935a4db9c8380bab90f83400a
Fagun Bhavsar <github.fagun@gmail.com>
2020-01-15 01:31:00 +0530

    Completed equality matchers

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -15,7 +15,29 @@ RSpec.describe 'equality matchers' do
     it 'both data and types' do
       expect(a).not_to eql(3)
       expect(b).not_to eql(3.0)
-      expect(c).not_to eql('3')
+      #expect(c).not_to eql('3')
+
+      expect(a).not_to eql(b)
+
+      expect(a).to eql(3.0)
+      expect(b).to eql(3)
+    end
+
+    describe 'equal and be matchers' do
+      let(:c) { [1,2,3] }
+      let(:d) { [1,2,3] }
+      let(:e) { c }
+
+      it 'equal cares about object identity' do
+        expect(c).to eq(d)
+        expect(c).to eql(d)
+
+        expect(c).to equal(e)
+        expect(c).to be(e)
+
+        expect(c).not_to equal(d)
+        expect(c).not_to equal([1,2,3])
+      end
     end
   end
 end
