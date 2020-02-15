661c5d86be612cd8eb2c7be23de7df5ce838c8db
Fagun Bhavsar <github.fagun@gmail.com>
2020-02-15 12:55:42 +0530

    Truthy Falsy test spec

---
+++ spec/be_matchers_spec.rb
@@ -0,0 +1,22 @@
+#falsy - false, nil
+# truthy - everything else except falsy
+
+RSpec.describe 'be matchers' do
+  it 'test for truthiness/falsyness' do
+    expect(true).to be_truthy
+    expect(false).to be_falsy
+    expect(nil).to be_truthy
+    expect(1).to be_truthy
+    expect(1.3).to be_truthy
+    expect(12345678.34567990).to be_truthy
+    expect([]).to be_truthy
+    expect([1,2,3]).to be_truthy
+    expect({}).to be_truthy
+  end
+
+  it 'test for nil' do
+    my_hash = {a: 2}
+    expect(my_hash[:b]).to be_nil
+    expect(my_hash[:c]).to be(nil)
+  end
+end
