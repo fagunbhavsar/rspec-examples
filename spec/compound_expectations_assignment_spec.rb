32aa8281863516cac65362772151b4fbefdc8328
Fagun Bhavsar <apple@Apples-MacBook-Pro.local>
2020-11-23 23:04:36 +0530

    Completed compound expectations assignment

---
+++ spec/compound_expectations_assignment_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe 'firetruck' do
+  it 'can multiple matchers on a single line' do
+    expect(subject).to start_with('fire').and end_with('truck')
+  end
+
+  it { is_expected.to start_with('fire').and end_with('truck') }
+end
+
+RSpec.describe 20 do
+  it 'can validate whether it is an even number and twice to the number 10' do
+    expect(subject).to be_even
+  end
+
+  it { is_expected.to be_even }
+end
+
+RSpec.describe [4, 8, 15, 16, 23, 42] do
+  it 'can validate whether array has value 42' do
+    expect(subject).to include(42).and start_with(4,8,15)
+  end
+
+  it { is_expected.to include(42).and start_with(4,8,15) }
+
+end
